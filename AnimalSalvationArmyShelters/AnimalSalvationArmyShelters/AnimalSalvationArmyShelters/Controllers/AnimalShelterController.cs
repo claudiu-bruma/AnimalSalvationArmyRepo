780f8335fed20aa892e184cec9d9c271c306a903
claudiu buma <claudiu.bruma@gmail.com>
2018-08-24 08:48:09 +0300

    Added initial files

---
+++ AnimalSalvationArmyShelters/AnimalSalvationArmyShelters/AnimalSalvationArmyShelters/Controllers/AnimalShelterController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnimalSalvationArmyShelters.Controllers
+{
+    public class AnimalShelterController : Controller
+    {
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ AnimalSalvationArmyShelters/AnimalSalvationArmyShelters/AnimalSalvationArmyShelters/Controllers/ShelterWorkerController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnimalSalvationArmyShelters.Controllers
+{
+    public class ShelterWorkerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
